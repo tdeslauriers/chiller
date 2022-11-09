a1198aad7d081d01551dad9898dd727caf3e25df
tdeslauriers <thomas.deslauriers@gmail.com>
2022-11-09 07:59:50 -0600

    exporting models

---
+++ model.go
@@ -0,0 +1,53 @@
+package main
+
+type User struct {
+	Id             int64           `json:"id"`
+	Username       string          `json:"username"`
+	Password       string          `json:"password"`
+	Firstname      string          `json:"firstname"`
+	Lastname       string          `json:"lastname"`
+	DateCreated    []int           `json:"dateCreated"`
+	Enabled        bool            `json:"enabled"`
+	AccountExpired bool            `json:"accountExpired"`
+	AccountLocked  bool            `json:"accountLocked"`
+	Birthday       []int           `json:"birthday"`
+	UserRoles      []UserRoles     `json:"userRoles"`
+	UserAddresses  []UserAddresses `json:"userAddresses"`
+	UserPhones     []UserPhones    `json:"userPhones"`
+}
+
+type UserRoles struct {
+	Id   int64 `json:"id"`
+	Role Role  `json:"role"`
+}
+
+type Role struct {
+	Id          int64  `json:"id"`
+	Role        string `json:"role"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type UserAddresses struct {
+	Id      int64   `json:"id"`
+	Address Address `json:"address"`
+}
+
+type Address struct {
+	Id      int64  `json:"id"`
+	Address string `json:"address"`
+	City    string `json:"city"`
+	State   string `json:"state"`
+	Zip     string `json:"zip"`
+}
+
+type UserPhones struct {
+	Id    int64 `json:"id"`
+	Phone Phone `json:"phone"`
+}
+
+type Phone struct {
+	Id    int64  `json:"id"`
+	Phone string `json:"phone"`
+	Type  string `json:"type"`
+}
